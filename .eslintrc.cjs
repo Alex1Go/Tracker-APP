b08b2f38b9039dac88d341e9e9d1037270b2a33c
Oleksii Hostryk <125027886+Alex1Go@users.noreply.github.com>
2025-08-05 18:35:45 +0300

    Updates

---
+++ .eslintrc.cjs
@@ -0,0 +1,26 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ['react'],
+  rules: {
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+}
---
+++ assets/react-transition-group-BZ6qa-kH.js
@@ -0,0 +1,1 @@
+import"./react-C1twzKfK.js";
