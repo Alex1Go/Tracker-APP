d512f08ee57ffbb9215e919e65afdf337fe17d4b
Oleksii Hostryk <125027886+Alex1Go@users.noreply.github.com>
2025-08-05 18:22:38 +0300

    change config

--- vite.config.js
+++ vite.config.js
@@ -59,4 +59,17 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/Tracker-APP/',
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0];
+          }
+        }
+      }
+    }
+  }
 })
